15303a0feedf63ac6e96301d7d8cd71f9150f30a
OyinDee <99656110+OyinDee@users.noreply.github.com>
2024-07-31 17:47:27 +0100

    removed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,13 @@ import Messages from './components/Messages';
 import './App.css'
 function App() {
   return (
-    <>
-  <div className='my-0'>
-    <figure className="text-end p-2 my-0" style={{backgroundColor: "#05163d", color: "white", fontFamily:"Lobster"}}>
-    <blockquote className="blockquote" >
-    <p className= "pe-5 pt-2" style={{backgroundColor: "#05163d", color: "white", fontFamily:"Lobster"}}>Wispa</p>
-    </blockquote>
-    </figure>
-  </div>  
+    <> 
     <Routes>
       <Route path= '/create' element= {<Signup/>}/>
       <Route path = '/send/:usernamee' element ={<Send/>}/>
       <Route path = '/get' element ={<Messages/>}/>
       <Route path= '/' element= {<Login/>}/>
       <Route path= '/sent' element= {<Sent/>}/>
-
     </Routes>
     </>
   )
